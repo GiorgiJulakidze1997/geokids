eef3961d5385809ce65413e8c756b8e611c840c8
Cotne Basiashvili <basiashvilic@gmail.com>
2024-02-01 16:21:04 +0400

    added aboutPage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,30 @@
 // import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
     <div>
       <nav>
-        <ul className="flex">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/classPage">Class Page</Link></li>
-          <li><Link to="/playerPage">Player Page</Link></li>
-          <li><Link to="/matchesPage">Matches Page</Link></li>
-          <li><Link to="/aboutPage">About Page</Link></li>
-          <li><Link to="/contactPage">Contact Page</Link></li>
+        <ul className="flex justify-between">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/classPage">Class Page</Link>
+          </li>
+          <li>
+            <Link to="/playerPage">Player Page</Link>
+          </li>
+          <li>
+            <Link to="/matchesPage">Matches Page</Link>
+          </li>
+          <li>
+            <Link to="/aboutPage">About Page</Link>
+          </li>
+          <li>
+            <Link to="/contactPage">Contact Page</Link>
+          </li>
         </ul>
       </nav>
     </div>
