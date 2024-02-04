d0d97f532afffbd9d6abc1163b5389f4def4ea5f
Cotne Basiashvili <basiashvilic@gmail.com>
2024-02-05 00:29:57 +0400

    added some styles

--- src/components/navImg/NavImg.tsx
+++ src/components/navImg/NavImg.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import homeIcon from "/icons/home-icon.png";
 import arrowIcon from "/icons/arrow.png";
 import { Link } from "react-router-dom";
@@ -9,12 +8,12 @@ type Props = {
 
 const NavImg = ({ title }: Props) => {
   return (
-    <div className="max-w-[1920px] h-[400px] bg-[url(/assets/nav-img-bg.webp)] bg-no-repeat bg-cover bg-center flex items-center justify-center mx-auto">
-      <div className="flex flex-col items-center gap-4">
-        <h2 className="text-[60px] text-white font-semibold uppercase">
+    <div className="max-w-[1920px] h-[170px] bg-[url(/assets/nav-img-bg.webp)] bg-no-repeat bg-cover bg-center flex items-center justify-center mx-auto md:h-[300px] xl:h-[400px]">
+      <div className="flex flex-col items-center gap-1 md:gap-3 xl:gap-4">
+        <h2 className="text-[30px] text-white font-semibold uppercase md:text-[40px] xl:text-[60px]">
           {title}
         </h2>
-        <p className="flex items-center text-white gap-1">
+        <p className="flex items-center text-white gap-1 text-xs md:text-base xl:text-lg">
           <Link to={"/"} className="flex items-center gap-1">
             <img src={homeIcon} alt="home icon img" />
             Home
