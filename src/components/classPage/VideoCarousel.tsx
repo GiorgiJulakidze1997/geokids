3cc6b391e12405c51fe507035198cb3ebff03bdc
tornike2002 <toko.toka2000@gmail.com>
2024-02-05 17:13:57 +0400

    added new component with carousel

---
+++ src/components/classPage/VideoCarousel.tsx
@@ -0,0 +1,31 @@
+import { Carousel } from "antd";
+
+const contentStyle: React.CSSProperties = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
+const VideoCarousel = () => {
+  return (
+    <div>
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default VideoCarousel;
--- src/components/pages/ClassPage.tsx
+++ src/components/pages/ClassPage.tsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 
+import VideoCarousel from "../classPage/VideoCarousel";
 import NavImg from "../navImg/NavImg";
 import ClassPhotos from "../photos/ClassPhotos";
 
 export default function ClassPage() {
   return (
     <div>
+      <VideoCarousel />
       <NavImg title="class" />
       <ClassPhotos />
     </div>
